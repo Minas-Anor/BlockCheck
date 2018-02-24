3fdc17c06c624693eae396134f2f3206e0608365
Yash Sharma <yash4688@gmail.com>
2018-02-24 10:53:09 +0530

    Add transaction method

---
+++ blockchain.py
@@ -0,0 +1,28 @@
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+    
+    def new_block(self):
+
+        pass
+
+    def new_transaction(self, sender, recipient, amount):
+        
+        self.current_transactions.append({
+            'sender' : sender,
+            'recipient' : recipient,
+            'amount' : amount
+        })
+
+        return self.last_block['index'] + 1
+
+    @staticmethod
+    def hash(block):
+
+        pass
+
+    @property
+    def last_block(self):
+
+        pass
