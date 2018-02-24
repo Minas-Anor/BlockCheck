45f14d7fd04e8dc5f6eb9dcdfbe83c7a9a65721a
Yash Sharma <yash4688@gmail.com>
2018-02-24 11:05:12 +0530

    Hashing and Creating Blocks

--- blockchain.py
+++ blockchain.py
@@ -1,28 +1,45 @@
+import hashlib
+import json
+from time import time
+
 class Blockchain(object):
+
     def __init__(self):
         self.chain = []
         self.current_transactions = []
-    
-    def new_block(self):
 
-        pass
+        self.new_block(previous_hash = 1, proof = 100)
+    
+    def new_block(self, proof, previous_hash = None):
+
+        block = {
+            'index' : len(self.chain) + 1,
+            'timestamp' : time(),
+            'transactions' : self.current_transactions,
+            'proof' : proof,
+            'previous_hash' : previous_hash or self.hash(self.chain[-1]), 
+        }
+
+        self.current_transactions = []
+
+        self.chain.append(block)
+        return block
 
     def new_transaction(self, sender, recipient, amount):
         
         self.current_transactions.append({
             'sender' : sender,
             'recipient' : recipient,
-            'amount' : amount
+            'amount' : amount,
         })
 
         return self.last_block['index'] + 1
 
     @staticmethod
     def hash(block):
-
-        pass
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
 
     @property
     def last_block(self):
-
-        pass
+        return self.chain[-1]
